78a878182ca8078d2663e066f7d73f7e6de66ef6
Yamamoto Kiyotaka <yamamotokiy@kawai.co.jp>
2022-02-07 16:41:07 +0900

    update: 3.3-1 Function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let a = [1, 2, 3, 4, 5];
-    let index = 10;
-    let element = a[index];
-    println!("The value of element is: {}", element);
+    println!("Hello, world!");
+    another_function();
+}
+
+fn another_function() {
+    println!("Anothor function.");
 }
