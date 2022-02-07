8264a6edcdc4af705d49664e05c5a0b3a5fdffe4
Yamamoto Kiyotaka <yamamotokiy@kawai.co.jp>
2022-02-07 14:23:20 +0900

    update: 3.2-7

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let a = [1, 2, 3, 4, 5];
-    let first = a[0];
-    let second = a[1];
-    println!("first: {}, second: {}", first, second);
+    let index = 10;
+    let element = a[index];
+    println!("The value of element is: {}", element);
 }
